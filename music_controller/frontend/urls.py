5c06eb3619a1e653eb7cefca8d75518041fde3fc
TrueRealHero <ub1nono@gmail.com>
2022-01-06 01:15:15 +0600

    react router dom changes needs to check

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index, name='Frontend')
+    path('', index, name='Frontend'),
+    path('join', index),
+    path('create', index)
+
 ]
