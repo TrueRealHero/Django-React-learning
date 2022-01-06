0ba37a63cb58a00b82e948b7b72e3af32dada34a
TrueRealHero <ub1nono@gmail.com>
2022-01-06 19:04:47 +0600

    play/pause buttons

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -18,7 +18,32 @@ export default class CreateRoomPage extends Component {
     }
 
     render() {
-        return <Grid container spacing={1}></Grid>;
-        
+        return <Grid container spacing={1}>
+            <Grid item xs={12} align='center'>
+                <Typography component='h4' variant='h4'>
+                    Create a Room
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <FormControl component='fieldset'>
+                    <FormHelperText>
+                        <div align='center'>
+                            Guest control of Playback State
+                        </div>
+                    </FormHelperText>
+                    <RadioGroup row defaultValue='true'>
+                        <FormControlLabel value='true' control={<Radio color='primary' />}
+                        label='Play/Pause'
+                        labelPlacement='bottom'
+                        />
+                        <FormControlLabel value='false' control={<Radio color='secondary' />}
+                        label='No Control'
+                        labelPlacement='bottom'
+                        />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+        </Grid>;
+
     }
 }
